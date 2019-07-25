942f8884176809ae953bf890b8319134561b293b
Cyrus E <devcyruse@gmail.com>
2019-07-25 07:58:04 -0400

    deploying files

---
+++ build/precache-manifest.af1d06141c4e4a54d85998261ca2f197.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395494a88d365a94d19c198ef6016559",
+    "url": "/react-app-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8cc9c918a703b0fe102d",
+    "url": "/react-app-monsters-rolodex/static/css/main.99b165af.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-app-monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8cc9c918a703b0fe102d",
+    "url": "/react-app-monsters-rolodex/static/js/main.d4fa783e.chunk.js"
+  },
+  {
+    "revision": "9d9cfdc838013c0e5aa2",
+    "url": "/react-app-monsters-rolodex/static/js/runtime~main.352ca324.js"
+  }
+]);
